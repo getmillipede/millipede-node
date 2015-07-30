64e30b0d105ebc0e9efd60018d2d9a7708335db1
Bertrand Marron <bertrand.marron@ionisx.com>
2015-07-30 03:47:09 +0200

    Add default values and validation tests

--- test/millipede.test.js
+++ test/millipede.test.js
@@ -6,6 +6,24 @@ var millipede   = require('../lib');
 // ## //
 
 describe('millipede', function () {
+    describe('default values', function () {
+        it('should default to 20 if the size is undefined', function () {
+            expect(millipede().size).to.equal(20);
+        });
+
+        it('should not be reversed by default', function () {
+            expect(millipede().reverse).to.be.false;
+        });
+    });
+
+    describe('validation', function () {
+        it('should throw if the size is too great', function () {
+            var run = function () { millipede(100001); };
+
+            expect(run).to.throw(millipede.MillipedeValidationError);
+        });
+    });
+
     describe('render', function () {
         it('should render a millipede of size 1', function () {
             expect(millipede(1).toString()).to.equal([
