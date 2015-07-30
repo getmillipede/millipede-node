838cd03bb3ca86c11e64ba2cde9edf250d79b767
Bertrand Marron <bertrand.marron@ionisx.com>
2015-07-30 02:28:58 +0200

    Update environment

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,7 +2,16 @@ module.exports = function (grunt) {
     require('time-grunt')(grunt);
     require('load-grunt-tasks')(grunt);
 
+    var config = {
+        artifacts: process.env.CIRCLE_ARTIFACTS || 'reports',
+        reports: process.env.CIRCLE_TEST_REPORTS || 'reports'
+    };
+
     grunt.initConfig({
+        c: config,
+
+        // ## //
+
         jscs: {
             all: {
                 files: {
@@ -34,7 +43,68 @@ module.exports = function (grunt) {
                     ]
                 }
             }
+        },
+
+        // ## //
+
+        jsdoc : {
+            dist : {
+                src: [
+                    'package.json',
+                    'README.md',
+                    'Gruntfile.js',
+                    'bin/millipede',
+                    'lib/**/*.js',
+                    'test/**/*.js',
+                    'index.js'
+                ],
+                options: {
+                    destination: 'doc',
+                    configure: 'jsdoc.conf.json'
+                }
+            }
+        },
+
+        // ## //
+
+        mochaTest: {
+            spec: {
+                options: {
+                    reporter: 'spec',
+                    require: [
+                        'test/bootstrap/node'
+                    ]
+                },
+                src: [
+                    'test/*.test.js'
+                ]
+            },
+
+            junit: {
+                options: {
+                    reporter: 'mocha-jenkins-reporter',
+                    reporterOptions: {
+                        'junit_report_path': '<%= c.reports %>/unit/junit.xml'
+                    },
+                    require: [
+                        'test/bootstrap/node'
+                    ]
+                },
+                src: [
+                    'test/*.test.js'
+                ]
+            },
+
+            coverage: {
+                options: {
+                    reporter: 'mocha-lcov-reporter',
+                    quiet: true,
+                    captureFile: '<%= c.artifacts %>/coverage.lcov'
+                },
+                src: ['test/**/*.js']
+            }
         }
+
     });
 
     grunt.registerTask('default', [
@@ -43,6 +113,15 @@ module.exports = function (grunt) {
 
     grunt.registerTask('test', [
         'jscs:all',
-        'jshint:all'
+        'jshint:all',
+        'mochaTest:spec',
+        'mochaTest:coverage'
+    ]);
+
+    grunt.registerTask('ci', [
+        'jscs:all',
+        'jshint:all',
+        'mochaTest:junit',
+        'mochaTest:coverage'
     ]);
 };
